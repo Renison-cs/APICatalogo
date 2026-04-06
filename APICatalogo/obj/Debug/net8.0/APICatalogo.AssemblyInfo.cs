67afe20d2d6fb89426fe71c39961568d8c10d8cf
Renison Silva Alves <misterreni10@gmail.com>
2026-04-06 09:22:32 -0300

    feat: Adicionando Logs aos controllers

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada122f79567b8e9b9f3417fd71a0bf81b894eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a46114e433c32c42cf2db8e1e8e12a6fdb48915b")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
